7ff201a4c5d719719c7559e5888bec7d80905469
0Huns <dudgns3864@gmail.com>
2023-04-02 16:22:36 +0900

    feat: Tts update & display set page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:flutter/material.dart';
 import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
 
@@ -11,29 +12,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const FirstPage(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(),
     );
   }
 }
 
-class FirstPage extends StatefulWidget {
-  const FirstPage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  _FirstPageState createState() => _FirstPageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _FirstPageState extends State<FirstPage> {
+class _HomePageState extends State<HomePage> {
+  FlutterTts flutterTts = FlutterTts();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text('CureEYE DEMO'),
           centerTitle: true,
+          backgroundColor: Colors.black,
           elevation: 0,
         ),
         body: GridView.count(
@@ -45,119 +47,105 @@ class _FirstPageState extends State<FirstPage> {
                 const EdgeInsets.only(bottom: 0, left: 15, top: 100, right: 15),
             children: [
               InkWell(
-                onTap: () {
-                  print("대체텍스트");
+                onTap: () async {
+                  await flutterTts.speak('포장지 인식');
                 },
                 onDoubleTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
+                    MaterialPageRoute(
+                        builder: (context) => const BarcodePage()),
                   );
                 },
-                splashColor: Colors.blue, // 탭할 때 버튼 주변에 파란색 원형 애니메이션 생성
+                highlightColor: Colors.blue, // 터치할 때 버튼 주변에 반짝거리는 애니메이션 생성
                 child: Container(
                   padding: const EdgeInsets.all(5.0),
-                  child: Image.asset("assets/images/camera_icon.jpg"),
+                  child: const Icon(
+                    Icons.qr_code_scanner,
+                    size: 160,
+                  ),
                 ),
               ),
               InkWell(
-                onTap: () {
-                  print("대체텍스트");
+                onTap: () async {
+                  await flutterTts.speak('알약 인식');
                 },
                 onDoubleTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
+                    MaterialPageRoute(
+                        builder: (context) => const BarcodePage()),
                   );
                 },
-                splashColor: Colors.blue, // 탭할 때 버튼 주변에 파란색 원형 애니메이션 생성
+                highlightColor: Colors.blue, // 탭할 때 버튼 주변에 파란색 원형 애니메이션 생성
                 child: Container(
                   padding: const EdgeInsets.all(5.0),
-                  child: Image.asset("assets/images/ocr.png"),
+                  child: const Icon(
+                    Icons.medication,
+                    size: 180,
+                  ),
                 ),
               ),
               InkWell(
-                onTap: () {
-                  print("대체텍스트");
+                onTap: () async {
+                  await flutterTts.speak('소리 설정');
                 },
                 onDoubleTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
+                    MaterialPageRoute(
+                        builder: (context) => const BarcodePage()),
                   );
                 },
-                splashColor: Colors.blue, // 탭할 때 버튼 주변에 파란색 원형 애니메이션 생성
+                highlightColor: Colors.blue, // 탭할 때 버튼 주변에 파란색 원형 애니메이션 생성
                 child: Container(
                   padding: const EdgeInsets.all(5.0),
-                  child: Image.asset("assets/images/mic.jpg"),
+                  child: const Icon(
+                    Icons.volume_up,
+                    size: 180,
+                  ),
                 ),
               ),
               InkWell(
-                onTap: () {
-                  print("대체텍스트");
+                onTap: () async {
+                  await flutterTts.speak('화면 설정');
                 },
                 onDoubleTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
+                    MaterialPageRoute(
+                        builder: (context) => const DisplayPage()),
                   );
                 },
-                splashColor: Colors.blue, // 탭할 때 버튼 주변에 파란색 원형 애니메이션 생성
+                highlightColor: Colors.blue, // 탭할 때 버튼 주변에 파란색 원형 애니메이션 생성
                 child: Container(
                   padding: const EdgeInsets.all(5.0),
-                  child: Image.asset("assets/images/setup.png"),
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  print("대체텍스트");
-                },
-                onDoubleTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
-                  );
-                },
-                splashColor: Colors.blue, // 탭할 때 버튼 주변에 파란색 원형 애니메이션 생성
-                child: Container(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Image.asset("assets/images/camera_icon.jpg"),
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  print("대체텍스트");
-                },
-                onDoubleTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const HomePage()),
-                  );
-                },
-                splashColor: Colors.blue, // 탭할 때 버튼 주변에 파란색 원형 애니메이션 생성
-                child: Container(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Image.asset("assets/images/camera_icon.jpg"),
+                  child: const Icon(
+                    Icons.settings_brightness,
+                    size: 180,
+                  ),
                 ),
               ),
             ]));
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class BarcodePage extends StatefulWidget {
+  const BarcodePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<BarcodePage> createState() => _BarcodePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _BarcodePageState extends State<BarcodePage> {
   String result = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('CureEYE DEMO'),
+        centerTitle: true,
+        backgroundColor: Colors.black,
       ),
       body: Center(
         child: Column(
@@ -182,6 +170,104 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class DisplayPage extends StatefulWidget {
+  const DisplayPage({Key? key}) : super(key: key);
+
+  @override
+  State<DisplayPage> createState() => _DisplayPageState();
+}
+
+class _DisplayPageState extends State<DisplayPage> {
+  FlutterTts flutterTts = FlutterTts();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CureEYE DEMO'),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: GridView.count(
+          crossAxisCount: 2, // 1개 행에 항목 3개씩
+          mainAxisSpacing: 0,
+          crossAxisSpacing: 0,
+          childAspectRatio: 1,
+          padding:
+              const EdgeInsets.only(bottom: 0, left: 15, top: 100, right: 15),
+          children: [
+            InkWell(
+              onTap: () async {
+                await flutterTts.speak('검은색');
+              },
+              onDoubleTap: () {
+                print("색상변환");
+              },
+              highlightColor: Colors.blue, // 탭할 때 버튼 주변에 파란색 원형 애니메이션 생성
+              child: Container(
+                padding: const EdgeInsets.all(5.0),
+                child: const Icon(
+                  Icons.contrast,
+                  color: Colors.black,
+                  size: 170,
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () async {
+                await flutterTts.speak('파란색');
+              },
+              onDoubleTap: () {
+                print("색상변환");
+              },
+              highlightColor: Colors.blue, // 탭할 때 버튼 주변에 파란색 원형 애니메이션 생성
+              child: Container(
+                padding: const EdgeInsets.all(5.0),
+                child: const Icon(
+                  Icons.contrast,
+                  color: Colors.blue,
+                  size: 170,
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () async {
+                await flutterTts.speak('빨간색');
+              },
+              onDoubleTap: () {
+                print("색상변환");
+              },
+              highlightColor: Colors.blue, // 탭할 때 버튼 주변에 파란색 원형 애니메이션 생성
+              child: Container(
+                padding: const EdgeInsets.all(5.0),
+                child: const Icon(
+                  Icons.contrast,
+                  color: Colors.red,
+                  size: 170,
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () async {
+                await flutterTts.speak('초록색');
+              },
+              onDoubleTap: () {
+                print("색상변환");
+              },
+              highlightColor: Colors.blue, // 탭할 때 버튼 주변에 파란색 원형 애니메이션 생성
+              child: Container(
+                padding: const EdgeInsets.all(5.0),
+                child: const Icon(
+                  Icons.contrast,
+                  color: Colors.green,
+                  size: 170,
+                ),
+              ),
+            ),
+          ]),
     );
   }
 }
